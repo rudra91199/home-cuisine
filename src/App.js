7be8291e32ada96c1f3823ed7433d742e3a51b69
rudra91199 <rskrrudra9111999@gmail.com>
2022-11-16 00:06:00 +0600

    notFound

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes ,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
 import Services from './Pages/Home/Services/Services';
 import Login from './Pages/Login/Login/Login';
@@ -7,20 +7,22 @@ import Home from './Pages/Home/Home/Home';
 import HeaderLogo from './Pages/Shared/HeaderLogo/HeaderLogo';
 import Foods from './Pages/Foods/Foods';
 import Footer from './Pages/Shared/Footer/Footer';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 
 function App() {
   return (
     <div className='app'>
-      <HeaderLogo/>
-      <Header/>
+      <HeaderLogo />
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/services' element={<Services />} />
         <Route path='/foods' element={<Foods />} />
         <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
+import notFound from '../../../images/notFound.jpg'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+                <img src={notFound} alt="" />
+            <div>
+                <h2>The page you are looking for can't be found.</h2>
+                <Link to={'/'}>Go Back </Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
